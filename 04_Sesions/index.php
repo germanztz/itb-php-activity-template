c97acbdbfdf118ff0814c665b4a3d7792b972863
germanztz <11229630+germanztz@users.noreply.github.com>
2026-03-09 17:12:45 +0000

    feat: millorar la gestió de sessions i formularis de login/logout

--- 04_Sesions/index.php
+++ 04_Sesions/index.php
@@ -25,33 +25,58 @@
 <body>
     <div class="container">
     <?php
+    session_start();
 
-        session_start();
+    if(isset($_SESSION['missatge'])) { 
+        // Si hi ha un missatge a la sessió, el mostrem i després el borrem
+        echo "<div><p>" . $_SESSION['missatge'] . "</p></div>";
+        unset($_SESSION['missatge']);
+    }
 
+    if(isset($_POST['action']) && $_POST['action'] == 'logout') {
+        // Si el usuari ha fet clic al botó de logout, tanquem la sessió
+        session_destroy();
+        // Redirigim a la pàgina de index després de tancar sessió
+        $_SESSION['missatge'] = "Has tancat sessió correctament.";
+        header('Location: index.php');
 
-        if(!isset($_SESSION['ID'])) { ?>
+    } else if(isset($_SESSION['username'])) { 
+        // Si el usuari ja està loguejat, mostrem el formulari de logout
+    ?>
 
     <h1>Logout form</h1>
-    <form action="login_proc.php" method="POST">
-        <pre><?php var_dump($_SESSION); ?></pre>
+    <form action="index.php" method="POST">
         <input type="hidden" name="action" value="logout">
         <button type="submit">Tancar sessió</button>
     </form> 
-
-    <?php } else { ?>
-
+    <?php 
+    
+    } else if(isset($_POST['usuari'])) {
+        // Si el formulari de login ha estat enviat, processem les dades
+        if ($_POST['usuari'] == 'user' && $_POST['contrasenya'] == 'r23r4y45u5u67i68o79o') {
+            // Si les credencials són correctes, guardem el nom d'usuari a la sessió
+            $_SESSION['username'] = 'user';
+            $_SESSION['missatge'] = "Benvingut, " . $_SESSION['username'] . "! Has iniciat sessió correctament.";
+        } else {
+            $_SESSION['missatge'] = "Usuari i/o paraula clau incorrectes!";
+        }
+        // Redirigim a la pàgina de index després de loguejar
+        header('Location: index.php');
+    
+    } else { 
+        // Si el usuari no està loguejat, mostrem el formulari de login.
+        ?>
     
     <h1>Login form</h1>
-    <form action="login_proc.php" method="POST">
+    <form action="index.php" method="POST">
         <input type="text" name="usuari" placeholder="Nom d'usuari" value="user" autofocus>
-        <input type="password" name="contrasenya" placeholder="Paraula clau" value="pass">
-        <input type="hidden" name="action" value="login">
+        <input type="password" name="contrasenya" placeholder="Paraula clau" value="r23r4y45u5u67i68o79o">
         <button type="submit">Entrar</button>
     </form> 
 
-
     <?php } ?>
     </div>
+    <pre>$_SESSION: <?php var_dump($_SESSION); ?></pre>
 
 </body>
 </html>
